99c714a22e191d9df5ed422b5d069ad0ec70eb0c
mrinalsinha2211 <145749474+mrinalsinha2211@users.noreply.github.com>
2024-01-06 10:41:34 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
+  
   res.send('Hello World!');
 });
+app.listen(port,()=>{
+  console.log(`server is running on port ${port}`);
+})
 
 //Using app.listen create your first express server on port 3000
 module.exports = app;
